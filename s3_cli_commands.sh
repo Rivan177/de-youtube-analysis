8886e0f7192ff793ae632291aba681ca08aa9a81
Ivan Villalba <rivan3@gmail.com>
2023-03-27 22:38:08 -0600

    modified CLI commands

--- s3_cli_commands.sh
+++ s3_cli_commands.sh
@@ -1,3 +1,16 @@
 
 # Command to copy all JSON files in current location to specified S3 bucket with specific folder structure
 aws s3 cp . s3://de-on-youtube-raw-useast2-ivande-dev/youtube/raw_statistics_reference_data/ --recursive --exclude "*" --include "*.json"
+
+# Using Hive-style partitioning: The paths include both the names of the partition keys and the values that each path represents.
+# To copy all data files to its own location.
+aws s3 cp CAvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=ca/
+aws s3 cp DEvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=de/
+aws s3 cp FRvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=fr/
+aws s3 cp GBvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=gb/
+aws s3 cp INvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=in/
+aws s3 cp JPvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=jp/
+aws s3 cp KRvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=kr/
+aws s3 cp MXvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=mx/
+aws s3 cp RUvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=ru/
+aws s3 cp USvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=us/
