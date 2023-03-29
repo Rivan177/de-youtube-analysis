55cd25acef9eaf00d60318a7883373d4c09c37bc
Ivan Villalba <rivan3@gmail.com>
2023-03-28 21:38:55 -0600

    modified lambda_function for pep8

--- lambda_function.py
+++ lambda_function.py
@@ -13,9 +13,10 @@ os_input_write_data_operation = os.environ['write_data_operation']
 def lambda_handler(event, context):
     # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    try:
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'],
+                                    encoding='utf-8')
 
+    try:
         # Creating DF from content
         df_raw = wr.s3.read_json('s3://{}/{}'.format(bucket, key))
 
@@ -35,5 +36,7 @@ def lambda_handler(event, context):
         return wr_response
     except Exception as e:
         print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        print('Error getting object {} from bucket {}. Make sure they exist \
+              and your bucket is in the same region as this function.'
+              .format(key, bucket))
         raise e
